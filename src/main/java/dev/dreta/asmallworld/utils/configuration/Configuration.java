06e4024db20ec955532f7ee4c7eec60844fcd8c4
Gabriel "Dreta" Leen <hello@dreta.dev>
2021-08-01 10:05:26 +0800

    feat: make {Configuration} more useful

--- src/main/java/dev/dreta/asmallworld/utils/configuration/Configuration.java
+++ src/main/java/dev/dreta/asmallworld/utils/configuration/Configuration.java
@@ -1,13 +1,40 @@
+/*
+ * A Small World is a curated 2.5D Minecraft experience.
+ * Copyright (C) 2021 Dreta / Gabriel Leen
+ *
+ * A Small World is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * A Small World is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with A Small World.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.dreta.asmallworld.utils.configuration;
 
 import dev.dreta.asmallworld.ASmallWorld;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import java.io.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Configuration {
     @Getter
@@ -87,5 +114,177 @@ public class Configuration {
             writer.close();
         }
         this.config = YamlConfiguration.loadConfiguration(this.file);
+    }
+
+    public Set<String> getKeys(boolean deep) {
+        return config.getKeys(deep);
+    }
+
+    public Map<String, Object> getValues(boolean deep) {
+        return config.getValues(deep);
+    }
+
+    public boolean contains(String path) {
+        return config.contains(path);
+    }
+
+    public boolean contains(String path, boolean ignoreDefault) {
+        return config.contains(path, ignoreDefault);
+    }
+
+    public boolean isSet(String path) {
+        return config.isSet(path);
+    }
+
+    public Object get(String path) {
+        return config.get(path);
+    }
+
+    public Object get(String path, Object def) {
+        return config.get(path, def);
+    }
+
+    public void set(String path, Object value) {
+        config.set(path, value);
+    }
+
+    public ConfigurationSection createSection(String path) {
+        return config.createSection(path);
+    }
+
+    public ConfigurationSection createSection(String path, Map<?, ?> map) {
+        return config.createSection(path, map);
+    }
+
+    public String getString(String path) {
+        return config.getString(path);
+    }
+
+    public String getString(String path, String def) {
+        return config.getString(path, def);
+    }
+
+    public int getInt(String path) {
+        return config.getInt(path);
+    }
+
+    public int getInt(String path, int def) {
+        return config.getInt(path, def);
+    }
+
+    public boolean getBoolean(String path) {
+        return config.getBoolean(path);
+    }
+
+    public boolean getBoolean(String path, boolean def) {
+        return config.getBoolean(path, def);
+    }
+
+    public double getDouble(String path) {
+        return config.getDouble(path);
+    }
+
+    public double getDouble(String path, double def) {
+        return config.getDouble(path, def);
+    }
+
+    public long getLong(String path) {
+        return config.getLong(path);
+    }
+
+    public long getLong(String path, long def) {
+        return config.getLong(path, def);
+    }
+
+    public List<?> getList(String path) {
+        return config.getList(path);
+    }
+
+    public List<?> getList(String path, List<?> def) {
+        return config.getList(path, def);
+    }
+
+    public List<String> getStringList(String path) {
+        return config.getStringList(path);
+    }
+
+    public List<Integer> getIntegerList(String path) {
+        return config.getIntegerList(path);
+    }
+
+    public List<Boolean> getBooleanList(String path) {
+        return config.getBooleanList(path);
+    }
+
+    public List<Double> getDoubleList(String path) {
+        return config.getDoubleList(path);
+    }
+
+    public List<Float> getFloatList(String path) {
+        return config.getFloatList(path);
+    }
+
+    public List<Long> getLongList(String path) {
+        return config.getLongList(path);
+    }
+
+    public List<Byte> getByteList(String path) {
+        return config.getByteList(path);
+    }
+
+    public List<Character> getCharacterList(String path) {
+        return config.getCharacterList(path);
+    }
+
+    public List<Short> getShortList(String path) {
+        return config.getShortList(path);
+    }
+
+    public List<Map<?, ?>> getMapList(String path) {
+        return config.getMapList(path);
+    }
+
+    public Vector getVector(String path) {
+        return config.getVector(path);
+    }
+
+    public Vector getVector(String path, Vector def) {
+        return config.getVector(path, def);
+    }
+
+    public OfflinePlayer getOfflinePlayer(String path) {
+        return config.getOfflinePlayer(path);
+    }
+
+    public OfflinePlayer getOfflinePlayer(String path, OfflinePlayer def) {
+        return config.getOfflinePlayer(path, def);
+    }
+
+    public ItemStack getItemStack(String path) {
+        return config.getItemStack(path);
+    }
+
+    public ItemStack getItemStack(String path, ItemStack def) {
+        return config.getItemStack(path, def);
+    }
+
+    public Color getColor(String path) {
+        return config.getColor(path);
+    }
+
+    public Color getColor(String path, Color def) {
+        return config.getColor(path, def);
+    }
+
+    public Location getLocation(String path) {
+        return config.getLocation(path);
+    }
+
+    public Location getLocation(String path, Location def) {
+        return config.getLocation(path, def);
+    }
+
+    public ConfigurationSection getSection(String path) {
+        return config.getConfigurationSection(path);
     }
 }
